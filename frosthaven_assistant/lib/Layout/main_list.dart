20f52ec100078012cba03996642e5a6aa842de19
Hans Möller <hans.moller@valagroup.com>
2022-06-29 10:51:46 +0300

    reverted to non local hero, reoderable draw for main list

--- frosthaven_assistant/lib/Layout/main_list.dart
+++ frosthaven_assistant/lib/Layout/main_list.dart
@@ -1,4 +1,4 @@
-
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:frosthaven_assistant/Layout/character_widget.dart';
@@ -7,37 +7,17 @@ import 'package:frosthaven_assistant/Model/campaign.dart';
 import 'package:frosthaven_assistant/Resource/game_state.dart';
 import 'package:frosthaven_assistant/Resource/scaling.dart';
 import 'package:local_hero/local_hero.dart';
+//import 'package:great_list_view/great_list_view.dart';
+//import 'package:reorderableitemsview/reorderableitemsview.dart';
+import 'package:reorderables/reorderables.dart';
 
+import '../Resource/action_handler.dart';
+import '../Resource/commands/reorder_list_command.dart';
 import '../Resource/game_methods.dart';
 import '../services/service_locator.dart';
 import 'monster_widget.dart';
 
 double tempScale = 0.8;
-
-class LocalHeroOverlay extends StatefulWidget {
-  const LocalHeroOverlay({
-    Key? key,
-    this.child,
-  }) : super(key: key);
-
-  final Widget? child;
-
-  @override
-  _LocalHeroOverlayState createState() => _LocalHeroOverlayState();
-}
-
-class _LocalHeroOverlayState extends State<LocalHeroOverlay> {
-  @override
-  Widget build(BuildContext context) {
-    return ClipRect(
-      child: Overlay(
-        initialEntries: <OverlayEntry>[
-          OverlayEntry(builder: (context) => widget.child!),
-        ],
-      ),
-    );
-  }
-}
 
 class Item extends StatelessWidget {
   final ListItemData data;
@@ -46,18 +26,40 @@ class Item extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double scale = getScaleByReference(context);
     late final Widget child;
+    late final double height;
     if (data is Character) {
       Character character = data as Character;
-      child = CharacterWidget(
-          key: Key(character.id), characterClass: character.characterClass);
+      child = CharacterWidget(key: Key(character.id), characterClass: character.characterClass);
+      height = 60 * scale; //TODO:cna I get implicit height?
+      //TODO put in ListItemData, and have it change depending on summons+monster instances
     } else if (data is Monster) {
       Monster monster = data as Monster;
       child = MonsterWidget(key: Key(monster.id), data: monster);
+      int standeeRows = 0;
+      if (monster.monsterInstances.value.length > 0) {
+        standeeRows = 1;
+      }
+      if (monster.monsterInstances.value.length > 4) {
+        standeeRows = 2;
+      }
+      height = 120 * tempScale * scale + standeeRows * 50;
+      //TODO put in ListItemData, and have it change depending on summons+monster instances
+    } else {
+      height = 0;
     }
-    return LocalHero(
+    /*return LocalHero(
       tag: child.key.toString(),
-      enabled: !_MainListState.isDragging,
+      child: child,
+    );*/
+    return child;
+    return AnimatedContainer(
+      height: height,
+      duration: const Duration(milliseconds: 500),
+      decoration: const BoxDecoration(
+        color: Colors.transparent,
+      ),
       child: child,
     );
   }
@@ -67,7 +69,6 @@ class MainList extends StatefulWidget {
   const MainList({Key? key}) : super(key: key);
 
   static void scrollToTop() {
-    //would it work if I turn off the hero animations temporarily?
     _MainListState.scrollToTop();
   }
 
@@ -78,7 +79,6 @@ class MainList extends StatefulWidget {
 class _MainListState extends State<MainList> {
   final GameState _gameState = getIt<GameState>();
   List<Widget> _generatedList = [];
-  static bool isDragging = false;
 
   void setCurrentTurn(int index) {
     //gray out all above, expire conditions/(un-expire if last current was lower down in list)
@@ -96,9 +96,8 @@ class _MainListState extends State<MainList> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        //alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+      //alignment: Alignment.center,
+      //width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
           image: DecorationImage(
               fit: BoxFit.cover,
@@ -114,6 +113,15 @@ class _MainListState extends State<MainList> {
             }));
   }
 
+  bool isSameContent(ListItemData a, ListItemData b) {
+    //todo: notify difference if height changes (i.e. monster/summon added
+    return true; //
+  }
+
+  bool isSameItem(ListItemData a, ListItemData b) {
+    return a.id == b.id;
+  }
+
   static void scrollToTop() {
     scrollController.animateTo(
       0,
@@ -123,34 +131,7 @@ class _MainListState extends State<MainList> {
     //scrollController.jumpTo(0);
   }
 
-  double getListHeight(){
-    double mainListWidth = getMainListWidth(context);
-    double scale = getScaleByReference(context);
-    double tempScale = 0.8;
-    double listHeight = 0;
-    for (int i = 0; i < _gameState.currentList.length; i++) {
-      var item = _gameState.currentList[i];
-      if (item is Character) {
-        listHeight += 64; //TODO: + summon list size
-      }
-      if (item is Monster) {
-        listHeight += 124 * tempScale;
-        if (item.monsterInstances.value.isNotEmpty) {
-          double listWidth = 0;
-          for (var monsterInstance in item.monsterInstances.value) {
-            listWidth += MonsterBox.getWidth(scale, monsterInstance);
-          }
-
-          double rows = listWidth.ceil() / mainListWidth.toInt();
-          listHeight += 34 * tempScale * (rows.ceil().toDouble());
-        }
-      }
-    }
-
-    return listHeight * 1.2 * scale;
-  }
-
-  int getItemsCanFitOneColumn(double listHeight) {
+  int getItemsCanFitOneColumn() {
     //too bad this has to be done
     bool canFit2Columns =
         MediaQuery.of(context).size.width >= getMainListWidth(context) * 2;
@@ -159,49 +140,115 @@ class _MainListState extends State<MainList> {
           .currentList.length; //don't wrap if no space. Probably not needed
     }
     double screenHeight = MediaQuery.of(context).size.height;
-    if(listHeight > 2 * screenHeight) {
-      //just divide equally
-      return (_gameState.currentList.length/2).ceil();
+    double listHeight = 0;
+    double scale = getScaleByReference(context);
+    double mainListWidth = getMainListWidth(context);
+
+    List<double> widgetPositions = [];
+    for (int i = 0; i < _gameState.currentList.length; i++) {
+      var item = _gameState.currentList[i];
+      if (item is Character) {
+        listHeight += 62; //TODO: + summon list size
+      }
+      if (item is Monster) {
+        listHeight += 130;
+        if (item.monsterInstances.value.isNotEmpty) {
+          double listWidth = 0;
+          for (var monsterInstance in item.monsterInstances.value) {
+            listWidth += MonsterBox.getWidth(scale, monsterInstance);
+          }
+
+          double rows = listWidth.toInt() / mainListWidth.toInt();
+          listHeight += 40 * (rows.floor().toDouble() + 1);
+        }
+      }
+      widgetPositions.add(listHeight);
     }
-    //TODO: put as many items as possible in first column
-    return (_gameState.currentList.length/2).ceil();
+    if (widgetPositions.last > 2 * screenHeight) {
+      //find center point
+      for (int i = 0; i < widgetPositions.length; i++) {
+        if (widgetPositions[i] > widgetPositions.last / 2) {
+          return i + 1;
+        }
+      }
+    } else {
+      //make all fit in screen of possible
+      for (int i = 0; i < widgetPositions.length; i++) {
+        if (widgetPositions[i] > screenHeight * 0.8) {
+          //TODO: use real values instead of leeway.
+          return i;
+        }
+      }
+    }
+    return _gameState.currentList.length;
   }
+
+
 
   List<Widget> generateChildren() {
+    //insert, remove and reorder. don't recreate. let's see about them animations.
+    //this causes items not to update their inner shit unless they are moved.
+    //I suppose I could do special hacks her since I know which items move where - add some hacky animation solution?
+    /*for (int i = 0; i < _gameState.currentList.length; i++) {
+      var data = _gameState.currentList[i];
+      bool found = false;
+
+      for(int j = 0; j <_generatedList.length; j++) {
+        var widget = _generatedList[j];
+        if(widget.data == data) {
+          //reorder
+          found = true;
+          if(i != j) {
+            _generatedList.insert(i, _generatedList.removeAt(j));
+          }
+          break;
+        }
+      }
+      if(!found) {
+        //create new
+        _generatedList.insert(i,
+          Item(data: data), //TODO: do I need the container or key?
+        );
+      }
+    }
+
+    //remove extras
+    if(_generatedList.length > _gameState.currentList.length){
+      for(var item in _generatedList) {
+        bool found = false;
+        for (var data in _gameState.currentList){
+          if(item.data == data) {
+            found = true;
+            break;
+          }
+        }
+        if (!found){
+          _generatedList.remove(item);
+        }
+      }
+    }*/
+
+
+
     _generatedList = List<Widget>.generate(
+      //TODO: this is probably super inefficient and also blocks animation
       _gameState.currentList.length,
-      (index) => Item(
-          key: Key(_gameState.currentList[index].id),
-          data: _gameState.currentList[index]),
+          (index) => Item(key: Key(_gameState.currentList[index].id), data: _gameState.currentList[index]),
     );
     return _generatedList;
   }
-
-
-  List<Widget> generateSomeChildren(int startIndex, int length) {
-    _generatedList = List<Widget>.generate(
-      length,
-          (index) => Item(
-          key: Key(_gameState.currentList[index+startIndex].id),
-          data: _gameState.currentList[index+startIndex]),
-    );
-    return _generatedList;
-  }
-
 
   Widget defaultBuildDraggableFeedback(
       BuildContext context, BoxConstraints constraints, Widget child) {
-    //disable the heroes from here? and then re-enable afterwards
-    isDragging = true;
     return Transform(
       transform: Matrix4.rotationZ(0),
       alignment: FractionalOffset.topLeft,
-
       child: Material(
         elevation: 6.0,
         color: Colors.transparent,
         borderRadius: BorderRadius.zero,
         child: Card(
+          //shadowColor: Colors.red,
             color: Colors.transparent,
             child: ConstrainedBox(constraints: constraints, child: child)),
       ),
@@ -211,106 +258,92 @@ class _MainListState extends State<MainList> {
   Widget buildList() {
     return Theme(
         data: Theme.of(context).copyWith(
-            //not needed
-            ),
-        child: ValueListenableBuilder<int>(
+          //not needed
+        ),
+        child: ValueListenableBuilder<int>( //todo listen for changes in list, not all commands?
             valueListenable: _gameState.updateList,
             builder: (context, value, child) {
-              //1 check can fit 2 columns
-              //2 if not, size the container to fix all items
-              //3 if can not fit, divide items equally and size containers to fit all items
-              //4 todo: reorder able column
-
-              bool canFit2Columns = MediaQuery.of(context).size.width >= getMainListWidth(context) * 2;
-              double listHeight = getListHeight();
-              int items = getItemsCanFitOneColumn(listHeight);
-
-              double heightFor2Columns = listHeight / 2;
-              if(heightFor2Columns < MediaQuery.of(context).size.height ) {
-                heightFor2Columns = MediaQuery.of(context).size.height;
-              }
-
-              double listWidth = getMainListWidth(context);
-              //List<Widget> children = generateChildren();
-
-              return  SingleChildScrollView(
-                padding: EdgeInsets.zero,
-               // margin: EdgeInsets.zero,
-                     controller: scrollController,
-                child: Container(
-                  padding: EdgeInsets.zero,
-                  margin: EdgeInsets.zero,
+              double scale = getScaleByReference(context);
+              return Container(
                   alignment: Alignment.topCenter,
-                    width: MediaQuery.of(context).size.width,
-                    height: canFit2Columns? heightFor2Columns: listHeight,
-                      child: LocalHeroOverlay(
+                  child: Scrollbar(
+                    controller: scrollController,
+                    //child: LocalHeroScope(
+                    // duration: const Duration(milliseconds: 300),
+                    // curve: Curves.easeInOut,
+                    child: ReorderableWrap(
+                      runAlignment: WrapAlignment.start,
+                      scrollAnimationDuration: Duration(milliseconds: 500),
+                      reorderAnimationDuration: Duration(milliseconds: 500),
+                      maxMainAxisCount: getItemsCanFitOneColumn(),
 
-                        child:SizedBox(
-                          width:  MediaQuery.of(context).size.width,
-                            child:Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                            width:  listWidth,
-                            child: Column(
-                              //scrollAnimationDuration: Duration(milliseconds: 500),
-                              //reorderAnimationDuration: Duration(milliseconds: 500),
-                              //maxMainAxisCount: getItemsCanFitOneColumn(),
-                              verticalDirection: VerticalDirection.down,
-                              mainAxisSize: MainAxisSize.min,
+                      direction: Axis.vertical,
+                      //scrollDirection: Axis.horizontal,
+                      //ignorePrimaryScrollController: true,
+                      buildDraggableFeedback: defaultBuildDraggableFeedback,
+                      needsLongPressDraggable: true,
+                      controller: scrollController,
+                      onReorder: (int oldIndex, int newIndex) {
+                        //setState(() {
+                        _gameState.action(ReorderListCommand(newIndex, oldIndex));
+                        //_gameState.currentList.insert(newIndex,
+                        //    _gameState.currentList.removeAt(oldIndex));
+                        //});
+                      },
+                      children: generateChildren(),
+                    ),
+                    /*AutomaticAnimatedListView<ListItemData>(
+                      animator: const DefaultAnimatedListAnimator(
+                          //dismissIncomingDuration: Duration(milliseconds: 1000),
+                          //reorderDuration: Duration(milliseconds: 2000),
 
-                              //buildDraggableFeedback: defaultBuildDraggableFeedback,
-                              //needsLongPressDraggable: true,
-                              /*onReorder: (int oldIndex, int newIndex) {
-                                //TODO: check draggable example from local_hero instead of this combo that doesn't work properly anyway
-                                Future.delayed(Duration(milliseconds: 10), () {
-                                  isDragging = false;
-                                  //the next sort, the moved object comes from wrong position.
-
-                                });
-                                _gameState
-                                    .action(ReorderListCommand(newIndex, oldIndex));
-                              },*/
-
-                              children: generateSomeChildren(0, items)// children.sublist(0,items),
-                            ),),
-                            canFit2Columns?
-                            SizedBox(
-                              width:  listWidth,
-                              child:Column(
-                              verticalDirection: VerticalDirection.down,
-                              mainAxisSize: MainAxisSize.min,
-                              //buildDraggableFeedback: defaultBuildDraggableFeedback,
-                              //needsLongPressDraggable: true,
-                              /*onReorder: (int oldIndex, int newIndex) {
-                                Future.delayed(Duration(milliseconds: 10), () {
-                                  isDragging = false;
-                                  //the next sort, the moved object comes from wrong position.
-                                });
-                                _gameState
-                                    .action(ReorderListCommand(newIndex, oldIndex));
-                              },*/
-
-                              children: generateSomeChildren(items, _gameState.currentList.length-items),// children.sublist(items,children.length),
-                            ))
-                                :Container(
-                              padding: EdgeInsets.zero,
-                              margin: EdgeInsets.zero,
+                          //dismissIncomingDuration: const Duration(milliseconds: 150),
+                          //resizeDuration: const Duration(milliseconds: 200),
+                          ),
+                      list: _gameState.currentList,
+                      comparator: AnimatedListDiffListComparator<ListItemData>(
+                          sameItem: (a, b) => isSameItem(a, b),
+                          sameContent: (a, b) => isSameContent(a, b)),
+                      itemBuilder: (context, item, data) => data.measuring
+                          ? Container(
+                              color: Colors.transparent,
+                              height:
+                                  item is Monster ? 120 * tempScale* scale : 60 * scale,
+                              //TODO: these are for smooth animations. need to be same size as the items
+                              //margin: const EdgeInsets.all(5), height: 60
                             )
-                          ],
-                        )
+                          : Item(data: item),
+                      listController: controller,
+                      scrollController: scrollController,
+                      addLongPressReorderable: true,
+
+                      /*reorderModel: AnimatedListReorderModel(
 
 
-                      )
-                      ),
-                //Container()
-              //])
-              )
-              );
+                onReorderStart: (index, dx, dy) {
+                  // only monster+character items can be reordered
+                  return true;
+                },
+                onReorderMove: (index, dropIndex) {
+                  // pink-colored items cannot be swapped
+                  return true;//list[dropIndex].color != 3;
+                },
+                onReorderComplete: (index, dropIndex, slot) {
+                  _gameState.currentList.insert(dropIndex, _gameState.currentList.removeAt(index));
+                  return true;
+                },
+              ),*/
+
+                      reorderModel: AutomaticAnimatedListReorderModel(
+                          _gameState.currentList),
+                    ),*/
+                    //)
+                  ));
             }));
   }
 
   static final scrollController = ScrollController();
+
+//use to animate to position in list:
+//final controller = AnimatedListController();
 }
