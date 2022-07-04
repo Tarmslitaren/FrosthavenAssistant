8c7b37b4b123bfc12b651bfbaaeb7894dc4ab1c6
Hans Möller <hans.moller@valagroup.com>
2022-07-04 16:39:39 +0300

    set campaign. sort scenarios by nr

--- frosthaven_assistant/lib/Layout/menus/select_scenario_menu.dart
+++ frosthaven_assistant/lib/Layout/menus/select_scenario_menu.dart
@@ -19,9 +19,38 @@ class _SelectScenarioMenuState extends State<SelectScenarioMenu> {
   @override
   initState() {
     // at the beginning, all items are shown
-    _foundScenarios = _gameState.modelData.value[_gameState.currentCampaign.value]!.scenarios.keys.toList();
-    _foundScenarios.sort((a, b) => a.compareTo(b)); //TODO: sort by nrs
+    setCampaign(_gameState.currentCampaign.value);
     super.initState();
+  }
+
+  int? findNrFromScenarioName(String scenario){
+    String nr = scenario.substring(1);
+    for(int i = 0; i < nr.length; i++){
+      if (nr[i] == ' ') {
+        nr = nr.substring(0, i);
+        int? number = int.tryParse(nr);
+        return number;
+      }
+    }
+
+    return null;
+  }
+
+  void setCampaign(String campaign) {
+    //TODO:clear search
+    _gameState.currentCampaign.value = campaign;
+    _foundScenarios = _gameState
+        .modelData.value[_gameState.currentCampaign.value]!.scenarios.keys
+        .toList();
+    _foundScenarios.sort((a, b) {
+
+      int? aNr = findNrFromScenarioName(a);
+      int? bNr = findNrFromScenarioName(b);
+      if(aNr != null && bNr != null){
+        return aNr.compareTo(bNr);
+      }
+      return a.compareTo(b);
+    });
   }
 
   // This function is called whenever the text field changes
@@ -29,9 +58,12 @@ class _SelectScenarioMenuState extends State<SelectScenarioMenu> {
     List<String> results = [];
     if (enteredKeyword.isEmpty) {
       // if the search field is empty or only contains white-space, we'll display all
-      results = _gameState.modelData.value[_gameState.currentCampaign]!.scenarios.keys.toList();
+      results = _gameState
+          .modelData.value[_gameState.currentCampaign.value]!.scenarios.keys
+          .toList();
     } else {
-      results = _gameState.modelData.value[_gameState.currentCampaign]!.scenarios.keys
+      results = _gameState
+          .modelData.value[_gameState.currentCampaign.value]!.scenarios.keys
           .toList()
           .where((user) =>
               user.toLowerCase().contains(enteredKeyword.toLowerCase()))
@@ -49,61 +81,87 @@ class _SelectScenarioMenuState extends State<SelectScenarioMenu> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      //color: Colors.transparent,
-       // shadowColor: Colors.transparent,
+        //color: Colors.transparent,
+        // shadowColor: Colors.transparent,
         margin: const EdgeInsets.all(20),
         child: Stack(children: [
-      Column(
-        children: [
-          const SizedBox(
-            height: 20,
-
-          ),
-          Container(
-            margin: const EdgeInsets.only(left:10, right: 10),
-            child:TextField(
-            onChanged: (value) => _runFilter(value),
-            decoration: const InputDecoration(
-                labelText: 'Set Scenario', suffixIcon: Icon(Icons.search)),
-          ),),
-          const SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            child: _foundScenarios.isNotEmpty
-                ? ListView.builder(
-                    itemCount: _foundScenarios.length,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text(_foundScenarios[index],
-                          style: TextStyle(fontSize: 18)
-
-                      ),
-                      onTap: () {
-                        _gameState
-                            .action(SetScenarioCommand(_foundScenarios[index]));
-                        Navigator.pop(context);
-                      },
-                    ),
-                  )
-                : const Text(
-                    'No results found',
-                    style: TextStyle(fontSize: 24),
-                  ),
-          ),
-        ],
-      ),
-      Positioned(
-          width: 100,
-          right: 2,
-          bottom: 2,
-          child: TextButton(
-              child: const Text(
-                'Close',
-                style: TextStyle(fontSize: 20),
+          Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              }))
-    ]));
+              Column(children: [
+                Text("Current Scenario: ${_gameState.currentCampaign.value}"),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      setCampaign("JotL");
+                    });
+                  },
+                  child: Text("Jaws of the Lion"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      setCampaign("Gloomhaven");
+                    });
+                  },
+                  child: Text("Gloomhaven"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      setCampaign("Crimson Scales");
+                    });
+                  },
+                  child: Text("Crimson Scales"),
+                ),
+              ]),
+              Container(
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  onChanged: (value) => _runFilter(value),
+                  decoration: const InputDecoration(
+                      labelText: 'Set Scenario',
+                      suffixIcon: Icon(Icons.search)),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: _foundScenarios.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: _foundScenarios.length,
+                        itemBuilder: (context, index) => ListTile(
+                          title: Text(_foundScenarios[index],
+                              style: TextStyle(fontSize: 18)),
+                          onTap: () {
+                            _gameState.action(
+                                SetScenarioCommand(_foundScenarios[index]));
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )
+                    : const Text(
+                        'No results found',
+                        style: TextStyle(fontSize: 24),
+                      ),
+              ),
+            ],
+          ),
+          Positioned(
+              width: 100,
+              right: 2,
+              bottom: 2,
+              child: TextButton(
+                  child: const Text(
+                    'Close',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }))
+        ]));
   }
 }
