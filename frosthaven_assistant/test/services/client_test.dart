951cb86f2f8ec9bf0c0a9a1c9a715bffeb44c8d7
Wonderplayer <roland.strakis@protonmail.com>
2023-03-25 15:22:43 +0200

    Remove public Send from client

--- frosthaven_assistant/test/services/client_test.dart
+++ frosthaven_assistant/test/services/client_test.dart
@@ -14,20 +14,4 @@ final _getIt = GetIt.instance;
 
 @GenerateNiceMocks([MockSpec<GameState>(), MockSpec<Communication>(), MockSpec<Connection>()])
 void main() {
-  test('does not send a message without active connections', () {
-    //arrange
-    const message = "TestMessage";
-    final stubGameState = MockGameState();
-    final mockCommunication = MockCommunication();
-    final mockConnection = MockConnection();
-    _getIt.registerFactory<GameState>(() => stubGameState);
-    _getIt.registerFactory<Communication>(() => mockCommunication);
-    _getIt.registerFactory<Connection>(() => mockConnection);
-
-    //act
-    _sut.send(message);
-
-    //assert
-    verifyNever(mockCommunication.sendTo(any, message));
-  });
 }
