c0058c6f5abd53284cf49ef10df2965e69bf9b9c
Hans Möller <hans.moller@valagroup.com>
2023-04-19 11:20:46 +0300

    change character name is command

---
+++ frosthaven_assistant/lib/Resource/commands/change_name_command.dart
@@ -0,0 +1,34 @@
+
+import 'package:collection/collection.dart';
+import 'package:frosthaven_assistant/Resource/state/character.dart';
+
+import '../../services/service_locator.dart';
+import '../action_handler.dart';
+import '../state/game_state.dart';
+
+class ChangeNameCommand extends Command {
+  ChangeNameCommand(this.name, this.characterId);
+  final String name;
+  final String characterId;
+
+
+  @override
+  void execute() {
+    GameState gameState = getIt<GameState>();
+    Character? character = gameState.currentList
+        .firstWhereOrNull((element) => element.id == name) as Character?;
+    if(character != null) {
+      character.characterState.display.value = name;
+    }
+
+  }
+
+  @override
+  void undo() {
+  }
+
+  @override
+  String describe() {
+    return "change character name";
+  }
+}
