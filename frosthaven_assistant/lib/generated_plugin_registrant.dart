970e0ff53b6ae97ef194d7747dad997f403d677c
Hans Möller <hansmoller@Larissas-MacBook-Air-6.local>
2023-07-02 15:15:20 +0300

    disable impeller engine for ios

---
+++ frosthaven_assistant/lib/generated_plugin_registrant.dart
@@ -0,0 +1,27 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:network_info_plus_web/network_info_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  NetworkInfoPlusPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
