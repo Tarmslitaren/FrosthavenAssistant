c7159a05df98ad0853c64160abe3f4a6908c1db8
Hans Möller <hansmoller@Larissas-Air-9.home>
2023-12-24 13:13:52 +0200

    revert to material2

--- frosthaven_assistant/lib/Layout/theme.dart
+++ frosthaven_assistant/lib/Layout/theme.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 //TODO: make a textTheme for each text type so that the size can be modified here (Germania One is wider)
 final theme = ThemeData(
+  useMaterial3: false,
   primarySwatch: Colors.lightBlue,
   fontFamily: 'Pirata',
 );
 
 final themeFH = ThemeData(
+  useMaterial3: false,
   primarySwatch: Colors.lightBlue,
   fontFamily: 'GermaniaOne',
 );
