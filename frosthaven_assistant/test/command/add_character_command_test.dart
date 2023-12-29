4ebfe5085598b6aee760595585fa691336452c8a
Hans Möller <hans.moller@valagroup.com>
2023-12-29 13:41:32 +0200

    use test data assets for testing

--- frosthaven_assistant/test/command/add_character_command_test.dart
+++ frosthaven_assistant/test/command/add_character_command_test.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:frosthaven_assistant/Resource/commands/add_character_command.dart';
+import 'package:frosthaven_assistant/Resource/enums.dart';
 import 'package:frosthaven_assistant/Resource/game_data.dart';
 import 'package:frosthaven_assistant/Resource/state/game_state.dart';
 import 'package:frosthaven_assistant/services/service_locator.dart';
@@ -8,33 +11,43 @@ import 'package:flutter_test/flutter_test.dart';
 late final GameState _gameState;
 
 Future<void> setUpAll() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   setupGetIt();
   _gameState = getIt<GameState>();
   //initialize game
   _gameState.init();
-  await getIt<GameData>().loadData("assets/data/");//todo: should use mock data
+  await getIt<GameData>().loadData("assets/testData/");//todo: should use mock data
   await _gameState.load();
+
+  //todo: test there is no side effects -> check that _gameState does not change aside from the changes
+  //todo: also test savestate: after each change test save+restore and that the state is equal
 
 }
 
 void tests() {
-  AddCharacterCommand command = AddCharacterCommand("Brute", "Arnold", 9);
+  AddCharacterCommand command = AddCharacterCommand("Hatchet", "Arnold", 9);
 
   command.execute();
 
   test("added ok", (){
     assert(_gameState.currentList.first is Character);
     assert(_gameState.currentList.first.id == "Arnold");
-    assert((_gameState.currentList.first as Character).characterClass.name == "Brute");
+    assert((_gameState.currentList.first as Character).characterClass.name == "Hatchet");
+    assert(_gameState.currentList.length == 1);
+    Character brute = GameMethods.getCurrentCharacters().first;
+    assert(brute.characterState.display.value == "Arnold");
+    assert(brute.characterState.level.value == 9);
+
   });
 
   test("description is ok", (){
-    assert(command.describe() == "Add Brute");
+    assert(command.describe() == "Add Hatchet");
+    assert(_gameState.commands.last?.describe() == "Add Hatchet");
   });
 }
 
 main() async {
-  TestWidgetsFlutterBinding.ensureInitialized();
 
   await setUpAll();
   tests();
