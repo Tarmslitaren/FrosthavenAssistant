16a24bbbe3bce627649282416fb810fe2dcf1f2e
Hans Möller <royalhasse@gmail.com>
2024-12-18 18:14:38 +0200

    update android project

--- frosthaven_assistant/android_bak/app/src/main/kotlin/com/tarmslitaren/frosthaven_assistant/MainActivity.kt
+++ frosthaven_assistant/android_bak/app/src/main/kotlin/com/tarmslitaren/frosthaven_assistant/MainActivity.kt
@@ -2,4 +2,5 @@ package com.tarmslitaren.frosthaven_assistant
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
