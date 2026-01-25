c74f3dfbf7318e8249d80ebd66bf789297e779f2
Hans Möller <royalhasse@gmail.com>
2026-01-25 17:32:50 +0200

    bump version

--- frosthaven_assistant/lib/Layout/menus/main_menu.dart
+++ frosthaven_assistant/lib/Layout/menus/main_menu.dart
@@ -109,7 +109,7 @@ class MainMenu extends StatelessWidget {
                   child: Stack(
                     children: [
                       Positioned(
-                          right: 6, bottom: 0, child: Text("Version 1.13.5"))
+                          right: 6, bottom: 0, child: Text("Version 1.13.7"))
                     ],
                   ),
                 ),
